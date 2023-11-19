61bacbc9a880612f350a8e4486592580b6fec442
Sonja <sonjaljatkovic@gmail.com>
2023-11-18 20:09:50 -0500

    Adding new ingredients in handlebars

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -24,20 +24,39 @@ document
     .querySelector(".add-recipe-form")
     .addEventListener("submit", newRecipeFormHandler);
 
-const renderIngredientsFields = () => {
+const ingredientsSectionEl = document.querySelector(".ingredients-section")
+
+const addIngredient = () => {
+    const newIngredients = document.createElement("div");
+
     const ingredientNameInputEl = document.createElement("input");
     ingredientNameInputEl.setAttribute("type", "text");
-    ingredientNameInputEl.setAttribute("id", "add-ingredient-name-input");
+    ingredientNameInputEl.setAttribute("class", "add-ingredient-name-value");
     ingredientNameInputEl.setAttribute("name", "add-ingredient-name");
+    ingredientNameInputEl.textContent = document.querySelector("#add-ingredient-name-input").value
+    newIngredients.appendChild(ingredientNameInputEl);
 
     const ingredientMeasurementInputEl = document.createElement("input");
     ingredientMeasurementInputEl.setAttribute("type", "text");
-    ingredientMeasurementInputEl.setAttribute("id", "add-ingredient-measurement-input");
+    ingredientMeasurementInputEl.setAttribute("class", "add-ingredient-measurement-value");
     ingredientMeasurementInputEl.setAttribute("name", "add-ingredient-measurement");
+    ingredientMeasurementInputEl.textContent = document.querySelector("#add-ingredient-measurement-input").value
+    newIngredients.appendChild(ingredientMeasurementInputEl);
 
     const measurementUnitInputEl = document.createElement("input");
     measurementUnitInputEl.setAttribute("type", "text");
-    measurementUnitInputEl.setAttribute("id", "add-measurement-unit-input");
+    measurementUnitInputEl.setAttribute("class", "add-measurement-unit-value");
     measurementUnitInputEl.setAttribute("name", "add-measurement-unit");
+    measurementUnitInputEl.textContent = document.querySelector("#add-measurement-unit-input").value
+    newIngredients.appendChild(measurementUnitInputEl);
 
+    ingredientsSectionEl.appendChild(newIngredients);
+
+    document.querySelector("#add-ingredient-name-input").value = "";
+    document.querySelector("#add-ingredient-measurement-input").value = "";
+    document.querySelector("#add-measurement-unit-input").value = "";
 }
+
+document
+    .querySelector("#add-ingredient-button")
+    .addEventListener("click", addIngredient);
