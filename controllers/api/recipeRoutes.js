aaf0970402ae833b8d4a72202a7a8200e009374a
leviathan <jake66martin@hotmail.com>
2023-11-18 19:27:42 -0500

    trying to work on update recipe route

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const { Recipe, Category, Users } = require("../../models");
-
-
+const { Recipe, Category, Users, Ingredients } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,46 +7,70 @@ router.get("/", async (req, res) => {
       include: [
         {
           model: Category,
-          attribute: "id",
+          attributes: ["id"],
         },
         {
           model: Users,
-          attribute: "id",
+          attribute: ["id"],
         },
       ],
     });
-if (recipeData) {
-
-    res.status(200).json(recipeData);
-} else {
-  return res.status(404).json(err);
-}
+    if (recipeData) {
+      res.status(200).json(recipeData);
+    } else {
+      return res.status(404).json(err);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-
-
-router.delete('/:id', async (req, res) => {
-
+router.delete("/:id", async (req, res) => {
   try {
     const delId = await Recipe.destroy({
       where: {
-        id: req.params.id
-      }
+        id: req.params.id,
+      },
     });
 
     if (delId) {
-      
-      return res.status(200).json({delId});
+      return res.status(200).json({ delId });
     } else {
-      
       return res.status(404).json(err);
     }
   } catch (err) {
-    
+    res.status(500).json(err);
+  }
+});
+
+
+router.put('/:id', async (req, res) => {
+  try {
+  const up =  await Recipe.update({
+    include: [{
+        model : Ingredients,
+        attributes: ["ingredient", "measurement", "unit"]
+    }]
+  },
+    {
+      name: req.body.name,
+      instructions: req.body.instructions,
+      ingredient: req.body.ingredient,
+      measurement: req.body.measurement,
+      unit: req.body.unit
+
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  if (up) {
+    return res.status(200).json(up)
+  }
+  } catch (err) {
     res.status(500).json(err);
   }
 });
